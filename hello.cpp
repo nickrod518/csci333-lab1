03eace985a4c9ff3016e4dc4f45bbfb143999b53
Nick Rodriguez <nickrod518@gmail.com>
2012-09-04 20:46:36 -0400

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#INCLUDE <IOSTREAM>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "hello world!" << endl << "another line" << endl;
+  cout << "goodbye!" << endl;
+  return 0;
+}
